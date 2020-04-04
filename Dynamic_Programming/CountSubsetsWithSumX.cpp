71b29c8a51cd06dd18512f21553be207e615a588
Mukul Madaan <mukul.madaan8@gmail.com>
2020-04-04 11:30:08 +0530

    Create CountSubsetsWithSumX.cpp

---
+++ Dynamic_Programming/CountSubsetsWithSumX.cpp
@@ -0,0 +1,19 @@
+int isSubsetSum(int set[], int n, int sum) 
+{
+    int DP[n + 1][sum + 1]; 
+    
+    for (int i = 0; i <= n; i++) 
+      DP[i][0] = 1;
+   
+    for (int i = 1; i <= sum; i++) 
+      DP[0][i] = 0;   
+      
+    for (int i = 1; i <= n; i++) { 
+       for (int j = 1; j <= sum; j++) { 
+          if(j < set[i - 1]) DP[i][j] = DP[i - 1][j]; 
+          if (j >= set[i - 1]) DP[i][j] = DP[i-1][j] + DP[i - 1][j - set[i - 1]]; 
+       } 
+     } 
+     
+    return DP[n][sum];
+}
