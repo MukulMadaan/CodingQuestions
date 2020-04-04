7e60d6493e1b430a7474a4230a867fe5b9c2548d
Mukul Madaan <mukul.madaan8@gmail.com>
2020-04-04 18:47:55 +0530

    Create CountSubsetsWithGivenDiff.cpp

---
+++ Dynamic_Programming/CountSubsetsWithGivenDiff.cpp
@@ -0,0 +1,25 @@
+int countSubsetDiff(int set[], int n, int diff) 
+{
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) sum += set[i];
+    
+    sum = (diff + sum)/2;
+   
+    int DP[n + 1][sum + 1]; 
+    
+    for (int i = 0; i <= n; i++) 
+      DP[i][0] = 1;
+   
+    for (int i = 1; i <= sum; i++) 
+      DP[0][i] = 0;   
+      
+    for (int i = 1; i <= n; i++) { 
+       for (int j = 1; j <= sum; j++) { 
+          if(j < set[i - 1]) DP[i][j] = DP[i - 1][j]; 
+          if (j >= set[i - 1]) DP[i][j] = DP[i-1][j] + DP[i - 1][j - set[i - 1]]; 
+       } 
+     } 
+     
+    return DP[n][sum];
+}
